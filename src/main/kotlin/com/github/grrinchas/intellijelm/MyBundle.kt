f10032debefada646ffefcef02ee4470ec667de1
GitHub Action <action@github.com>
2023-10-21 14:30:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/grrinchas/intellijelm/MyBundle.kt
+++ src/main/kotlin/com/github/grrinchas/intellijelm/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.grrinchas.intellijelm
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/grrinchas/intellijelm/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/grrinchas/intellijelm/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.grrinchas.intellijelm.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
